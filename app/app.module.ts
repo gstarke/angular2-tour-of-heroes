84e3a2bdf1642c5840252a84d777be81cbd2d0ef
Garret Starke <gstarke@claritycon.com>
2016-09-15 13:45:11 -0500

    Updates for the HTTP section.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,26 +1,35 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { AppComponent }        from './app.component';
-import { DashboardComponent }     from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
-import { routing } from './app.routing';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+import { routing }              from './app.routing';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-		routing
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    routing
   ],
   declarations: [
     AppComponent,
-		DashboardComponent,
+    DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
   ],
   providers: [
-    HeroService
+    HeroService,
   ],
   bootstrap: [ AppComponent ]
 })
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,25 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Hero} from './hero';
 
-import { HeroService } from './hero.service';
+import {HeroService} from './hero.service';
 @Component({
-  selector: 'my-hero-detail',
-	templateUrl: 'app/hero-detail.component.html',
-	styleUrls: ['app/hero-detail.component.css']
+    selector: 'my-hero-detail',
+    templateUrl: 'app/hero-detail.component.html',
+    styleUrls: ['app/hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-	constructor(
-		private heroService: HeroService,
-		private route: ActivatedRoute) {
-	}
-	ngOnInit(): void {
-		this.route.params.forEach((params: Params) => {
-			let id = +params['id'];
-			this.heroService.getHero(id)
-				.then(hero => this.hero = hero);
-		});
-	}
-	goBack(): void {
-		window.history.back();
-	}
+    hero: Hero;
+
+    constructor(private heroService: HeroService,
+                private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.route.params.forEach((params: Params) => {
+            let id = +params['id'];
+            this.heroService.getHero(id)
+                .then(hero => this.hero = hero);
+        });
+    }
+
+    goBack(): void {
+        window.history.back();
+    }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(this.goBack);
+    }
 }
